430cb3bbc6e6091540a85750d46c2ef0ac207dcb
Brandon Keefer <happysolucki@outlook.com>
2022-02-19 01:59:18 -0600

    status: refactored

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,6 +25,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveEffect}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -71,19 +72,18 @@ function App() {
             handleSelectOption={setClothesColor}
           />
         </div>
-        <div className={styles.characterWrapper}>
-          {' '}
-          <Character
-            body={body}
-            head={head}
-            face={face}
-            accessory={accessory}
-            skinColor={skinColor}
-            clothesColor={clothesColor}
-          />
-        </div>
       </MaxWidthWrapper>
-      <div className={styles.grayAccent}></div>
+      <div className={styles.characterWrapper}>
+        {' '}
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
     </main>
   );
 }
